f9e50d4f299b26d4471823105584f2f4dcebd995
Sheep-heee <sheephi0609@gmail.com>
2025-10-27 23:27:43 +0900

    first commit

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+// 프론트용 CSS
+add_action( 'wp_enqueue_scripts', function() {
+  // 자식 공통 CSS
+  wp_enqueue_style(
+    'tt4c-style',
+    get_stylesheet_directory_uri() . '/assets/build/style.css',
+    [],
+    filemtime( get_stylesheet_directory() . '/assets/build/style.css' )
+  );
+}, 20 );
